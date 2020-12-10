96ea6a19635b72be57e8b70ee9754483b5bdbe03
ms1004@student.uni-lj.si <majsavli@gmail.com>
2020-12-10 10:36:02 +0100

    first version of detector

---
+++ frame.m
@@ -0,0 +1,26 @@
+function frame( record )
+  % Summary of this function and detailed explanation goes here
+  
+  fileName = sprintf('1.0.0/%sm.mat', record);
+  
+  t = cputime();
+  m = 5;
+  idx = detector(fileName, m);
+  
+  fprintf('Running time: %f\n', cputime() - t);
+  
+  asciName = sprintf('%s.asc',record);
+  fid = fopen(asciName, 'wt');
+  
+  for i=1:size(idx, 2)
+      fprintf(fid,'0:00:00.00 %d N 0 0 0\n', idx(1, i) );
+  end
+  
+  fclose(fid);
+
+  % Now convert the .asc text output to binary WFDB format:
+  % wrann -r record -a qrs <record.asc
+  % And evaluate against reference annotations (atr) using bxb:
+  % bxb -r record -a atr qrs
+end
+
